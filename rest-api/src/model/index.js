84fb456345a227fbf85be2cc6f147dd10c2b6a32
Miikis <28997062+Miikis@users.noreply.github.com>
2019-07-26 11:16:42 -0700

    Add addUser method to model w/ unit-test
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

---
+++ rest-api/src/model/index.js
@@ -0,0 +1,22 @@
+import { hash } from "bcrypt"
+
+import db from "../data"
+
+const addUser = async ({ username, password }) => {
+    if (!username || !password)
+        throw Error(`A username and password are required!`)
+
+    const hashedPassword = await hash(password, 10)
+
+    const [id] = await db("users").insert({
+        username,
+        password: hashedPassword
+    })
+
+    return db("users")
+        .where("id", id)
+        .column("username")
+        .first()
+}
+
+export { addUser }

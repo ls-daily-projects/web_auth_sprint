3c3f3ef0bee5e497874ac14643543368e81a7053
Miikis <28997062+Miikis@users.noreply.github.com>
2019-07-26 11:50:21 -0700

    Fix seeds so generated passwords are hashed
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- rest-api/src/data/seeds/01_users.js
+++ rest-api/src/data/seeds/01_users.js
@@ -1,9 +1,14 @@
+import { hashSync } from "bcrypt"
+
 import { createMany, createUser } from "../utils"
 
 const users = [
     { id: 1, username: "icanhazdadjoke", password: "Password123!" },
     ...createMany(createUser)
-]
+].map(user => {
+    const hashedPassword = hashSync(user.password, 10)
+    return { ...user, password: hashedPassword }
+})
 
 export const seed = knex =>
     knex("users")
